bfb9b18cf04ffac225a4f939402f75aea110aa3b
pablo-dev989 <261613295+pablo-dev989@users.noreply.github.com>
2026-05-25 12:40:03 -0400

    producers y consumer parte 1/2

--- simple_consumer_conf/consumer.go
+++ simple_consumer_conf/consumer.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/confluentinc/confluent-kafka-go/kafka"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
 
 func main() {
 
@@ -13,4 +19,30 @@ func main() {
 		panic(err)
 	}
 
+	defer func() {
+		err := c.Close()
+		if err != nil {
+			log.Fatal("Failed to close consumer:", err)
+		}
+	}()
+
+	topic := "user-profiles"
+	err = c.Subscribe(topic, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Consumer started. Waiting for messages...")
+
+	for {
+		msg, err := c.ReadMessage(55 * time.Second)
+		if err != nil {
+			if err.(kafka.Error).Code() == kafka.ErrTimedOut {
+				continue
+			}
+			fmt.Println("Consume error:", err)
+			continue
+		}
+		fmt.Printf("Received message from %s: %s\n", msg.TopicPartition, string(msg.Value))
+	}
 }
